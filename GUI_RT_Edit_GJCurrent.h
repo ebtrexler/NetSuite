aba1047d763b536caf0b4d07c1034b1ccce40dc0
E. Brady Trexler, Ph.D <ebtrexler@gmail.com>
2026-02-21 21:57:01 -0500

    Update remaining gothamsci references to hudsonvalleysci in legacy files

--- GUI_RT_Edit_GJCurrent.h
+++ GUI_RT_Edit_GJCurrent.h
@@ -15,7 +15,7 @@ NetSuite: NetClamp/NetSim/NetFit Neural Network Modeling and Dynamic Clamp Softw
 	 You should have received a copy of the GNU General Public License
 	 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Please direct correspondence to ebtrexler _at_ gothamsci _dot_ com
+Please direct correspondence to ebtrexler _at_ hudsonvalleysci _dot_ com
 ///////////////////////////////////////////////////////////////////////////// */
 
 //---------------------------------------------------------------------------
@@ -39,7 +39,7 @@ Please direct correspondence to ebtrexler _at_ gothamsci _dot_ com
 //
 /// Implements a current for an electrical synapse
 /*!
-   @author E. Brady Trexler <ebtrexler (at) gothamsci.com>, 2011 - 2013
+   @author E. Brady Trexler <ebtrexler (at) hudsonvalleysci.com>, 2011 - 2013
 */
 class TGapJunctionCurrent : public TCurrent
 {
@@ -122,7 +122,7 @@ public:
 //
 /// GUI Editor for TGapJunctionCurrent
 /*!
-   @author E. Brady Trexler <ebtrexler (at) gothamsci.com>, 2011 - 2013
+   @author E. Brady Trexler <ebtrexler (at) hudsonvalleysci.com>, 2011 - 2013
 */
 class TGJCurrentForm : public TForm
 {
